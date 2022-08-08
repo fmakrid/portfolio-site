3904c43d477ae189f21ce2d0b0176a4db0291cde
Filippos Makridis <makridistaker@hotmail.gr>
2022-08-08 21:35:14 +0300

    FEATURE: Added working project view with redirect upon clicking

---
+++ src/components/Covid19.js
@@ -0,0 +1,13 @@
+import "./Covid19.css";
+
+function Covid19(){
+    return <div>
+        <h1>Covid19</h1>
+        <a href="http://covid19worldmap2.eu-central-1.elasticbeanstalk.com/">
+            <img src="./images/covid19.png" alt="Covid19"></img>
+        </a>
+        
+    </div>
+}
+
+export default Covid19;
---
+++ src/components/ProjectView.js
@@ -0,0 +1,10 @@
+import Covid19 from "./Covid19";
+
+function ProjectView(){
+    return <div>
+        <h1>ProjectView</h1>
+        <Covid19 />
+    </div>
+}
+
+export default ProjectView;
