27763bea53e7e6cb46cb9e1e38856271fb2fe077
Filippos Makridis <makridistaker@hotmail.gr>
2022-08-14 20:50:15 +0300

    FEATURE: Added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import ProjectView from './components/ProjectView';
+import Navigation from "./components/Navigation";
 
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <ProjectView />
     </div>
   );
