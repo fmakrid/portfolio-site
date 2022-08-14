b420370a82aaa0c7a77d69a3654ec6d6db91811e
Filippos Makridis <makridistaker@hotmail.gr>
2022-08-15 01:41:59 +0300

    FEATURE: Added active link highlight

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,19 +7,19 @@ function Navbar() {
     <div id="navbar">
       <h2>Makridis</h2>
       <nav id="navbuttons">
-          <NavLink to="/" activeStyle>
+          <NavLink to="/" activeStyle={{color: "gold"}}>
             Home
           </NavLink>
-          <NavLink to="/projects" activeStyle>
+          <NavLink to="/projects" activeStyle={{color: "gold"}}>
             My Work
           </NavLink>
-          <NavLink to="/about" activeStyle>
+          <NavLink to="/about" activeStyle={{color: "gold"}}>
             About
           </NavLink>
-          <NavLink to="/contact" activeStyle>
+          <NavLink to="/contact" activeStyle={{color: "gold"}}>
             Contact
           </NavLink>
-          <NavLink to="/resume" activeStyle>
+          <NavLink to="/resume" activeStyle={{color: "gold"}}>
             Resume
           </NavLink>
       </nav>
