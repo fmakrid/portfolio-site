3ac17e20d56d090216bd831c425531905c73f564
Filippos Makridis <makridistaker@hotmail.gr>
2022-08-16 19:13:13 +0300

    FEATURE: Added background image, text shadows and increased some margins on Home

---
+++ src/components/Background.js
@@ -0,0 +1,9 @@
+// import "./Background.css";
+
+// const background = new URL("../images/background2.jpg", import.meta.url);
+
+// const Background = () => {
+//   return <img src={background} alt="background" id="background" />;
+// };
+
+// export default Background;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Filippos Makridis</h1>
+      <h3>Computer Engineer | Web Developer | Software Engineer</h3>
+      <button id="viewprojectsbutton">View Projects</button>
     </div>
   );
 };
