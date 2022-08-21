9631eddafc9b926797497d526cea25005d09b73b
Filippos Makridis <makridistaker@hotmail.gr>
2022-08-21 22:13:44 +0300

    MAINTENANCE - BUGFIX: Fixed bug which displayed <a> tag on the bottom on #smallbuttons. Changed website Title, minor tweaks.

--- src/components/Covid19.js
+++ src/components/Covid19.js
@@ -1,7 +1,7 @@
 import "./Covid19.css";
 
 function Covid19(){
-    return <div>
+    return <div id="Covid19">
         <h1>Covid19</h1>
         <a href="http://covid19worldmap2.eu-central-1.elasticbeanstalk.com/">
             <img src="./images/covid19.png" alt="Covid19"></img>
--- src/components/Smallbuttons.js
+++ src/components/Smallbuttons.js
@@ -1,20 +1,24 @@
 const Smallbuttons = () => {
   return (
     <div id="smallbuttons">
-      <a
-        href="https://github.com/fmakrid"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={require("./github.png")} alt="Github" />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/filippos-makridis/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={require("./linkedin.png")} alt="LinkedIn" />
-      </a>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/fmakrid"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img id="github" src={require("./github.png")} alt="Github" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/filippos-makridis/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img id="linkedin" src={require("./linkedin.png")} alt="LinkedIn" />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
