8714e260c2ae71ed96269db7d3b7cbd5133047b8
Filippos Makridis <makridistaker@hotmail.gr>
2022-08-23 02:17:56 +0300

    Minor visual adjustments

--- src/components/ProjectView.js
+++ src/components/ProjectView.js
@@ -6,6 +6,11 @@ function ProjectView() {
       <ul>
         <li>
           <h3>Covid-19 World Map</h3>
+          <p>
+            An interactive world map that shows the covid-19 cases for each
+            country by coloring them accordingly after the user picks a date
+            from the calendar.
+          </p>
           <a href="http://covid19worldmap2.eu-central-1.elasticbeanstalk.com/">
             <img id="covid19" src={covid19} alt="Covid19"></img>
           </a>
