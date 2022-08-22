f6fc1079e0e2314cef7a7dc76a6b9778d8251b1c
Filippos Makridis <makridistaker@hotmail.gr>
2022-08-23 02:30:54 +0300

    Added link to View Projects button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../assets/css/Home.css";
+import { Link } from "react-router-dom";
 import Smallbuttons from "../components/Smallbuttons";
 
 const Home = () => {
@@ -7,8 +8,11 @@ const Home = () => {
     <div id="home">
       <h1 id="name">Filippos Makridis</h1>
       <h3 id="titles">Computer Engineer | Web Developer | Software Engineer</h3>
-      <button id="viewprojectsbutton">View Projects</button>
-      <Smallbuttons />      
+      <Link to="/Projects">
+        <button id="viewprojectsbutton">View Projects</button>
+      </Link>
+
+      <Smallbuttons />
     </div>
   );
 };
