483c4561d5221edd9fb20ec4414dafb2a7ac306f
Filippos Makridis <makridistaker@hotmail.gr>
2022-08-31 02:13:42 +0300

    FEATURE: Added animation on projects

--- src/components/Covid19.js
+++ src/components/Covid19.js
@@ -1,19 +1,30 @@
 import covid19 from "../assets/images/covid19.png";
-import {motion} from "framer-motion";
+import 'animate.css';
+
+
+
+
+
 
 function Covid19() {
-    return (
-        <div id="covid19">
-            <h3>Covid-19 World Map</h3>
-          <h3>(Thesis)</h3>
-          <motion.p class="firstLine">An interactive world map</motion.p>
-          <motion.p class="secondLine">That shows the covid-19 cases of each country by color</motion.p>
-          <motion.p class="thirdLine">Of each picked date.</motion.p>
-          <a href="http://covid19worldmap2.eu-central-1.elasticbeanstalk.com/">
-            <img id="covid19" src={covid19} alt="Covid19"></img>
-          </a>
-        </div>
-    )
+  return (
+    <div id="covid19">
+      <h3 class="projectTitle">
+        Covid-19 World Map
+        <br />
+        (Thesis)
+      </h3>
+
+      <p class="projectDescription ">
+        An interactive world map
+        <br /> That shows the covid-19 cases of each country by color
+        <br /> Of each picked date.
+      </p>
+      <a class="projectIcon" href="http://covid19worldmap2.eu-central-1.elasticbeanstalk.com/">
+        <img id="covid19" src={covid19} alt="Covid19"></img>
+      </a>
+    </div>
+  );
 }
 
 export default Covid19;
--- src/components/ProjectView.js
+++ src/components/ProjectView.js
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 import Covid19 from "./Covid19.js";
 
 function ProjectView() {
