1b3b96efab93628f44b408c18bb6a31e1e832618
Filippos Makridis <makridistaker@hotmail.gr>
2022-08-31 03:13:11 +0300

    Minor visual changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,17 +5,19 @@ const About = () => {
   return (
     <div id="about">
       <div id="bio">
+        <p>Greetings!</p>
         <p>
-          My name is Filippos Makridis and i'm a Computer Engineer graduate
+           My name is Filippos Makridis and i'm a Computer Engineer graduate
           (BaCpE) from the International Hellenic University.
         </p>
+
         <p>My skills include Software Engineering and Web Developing</p>
         <p>
           Some of the languages i have worked with are C++, Java, Python, HTML,
           CSS, JavaScript, SQL and frameworks including Node.js, jQuery for Web
           Development projects and my thesis.
         </p>
-        <p>Always enjoying learning new technologies and concepts!</p>
+        <p>If you want to contact me you can either go to Contact page of the site or click this <a href="/Contact">link</a></p>
       </div>
     </div>
   );
