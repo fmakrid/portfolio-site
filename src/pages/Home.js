d1f62bde7ea120ffeac2abad6ea53885c293d755
Filippos Makridis <makridistaker@hotmail.gr>
2022-09-30 01:24:22 +0300

    BUGFIX-MAINTENANCE: Fixed issue with <a> tag appearing outside the button by using onClick instead. Minor visual adjustments.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "../assets/css/Home.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Smallbuttons from "../components/Smallbuttons";
 import ResumeButton from "../components/ResumeButton";
 
 const Home = () => {
+  let navigate = useNavigate();
   return (
     <div id="home">
       <h1 id="name">Filippos Makridis</h1>
       <h3 id="titles">Computer Engineer | Web Developer | Software Engineer</h3>
       <ResumeButton />
-      <Link to="/Projects">
-        <button id="viewprojectsbutton">View Projects</button>
-      </Link>
+      <button id="viewprojectsbutton" onClick={() => navigate("/Projects")}>
+        View Projects
+      </button>
+
       <Smallbuttons />
     </div>
   );
