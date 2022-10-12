27c91bcfb51d196e360a321422cdad8647d2c0ee
Filippos Makridis <makridistaker@hotmail.gr>
2022-10-12 19:04:18 +0300

    Minor visual adjustments

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,24 +4,26 @@ import "../assets/css/Navbar.css";
 
 function Navbar() {
   return (
-    <div id="navbar">
-      <h2 id="surname">Makridis</h2>
-      <nav id="navbuttons">
-        <NavLink to="/" activestyle={{ color: "gold" }}>
-          Home
-        </NavLink>
-        <NavLink to="/projects" activestyle={{ color: "gold" }}>
-          My Work
-        </NavLink>
-        <NavLink to="/about" activestyle={{ color: "gold" }}>
-          About
-        </NavLink>
-        <NavLink to="/contact" activestyle={{ color: "gold" }}>
-          Contact
-        </NavLink>
-        <NavLink to="/resume" activestyle={{ color: "gold" }}>
-          Resume
-        </NavLink>
+    <div class="navbar">
+      <h2 class="brand-title">Makridis</h2>
+      <nav class="navbar-links">
+        <ul>
+          <NavLink to="/" activestyle={{ color: "gold" }}>
+            Home
+          </NavLink>
+          <NavLink to="/projects" activestyle={{ color: "gold" }}>
+            My Work
+          </NavLink>
+          <NavLink to="/about" activestyle={{ color: "gold" }}>
+            About
+          </NavLink>
+          <NavLink to="/contact" activestyle={{ color: "gold" }}>
+            Contact
+          </NavLink>
+          <NavLink to="/resume" activestyle={{ color: "gold" }}>
+            Resume
+          </NavLink>
+        </ul>
       </nav>
     </div>
   );
